0ddffe0ddfd09f37027f74dc6d1c17287c4026d3
Darshan p gohil <163631278+darshanpgohil@users.noreply.github.com>
2026-08-12 15:58:10 +0530

    practicle5

--- practicle5.py
+++ practicle5.py
@@ -4,7 +4,7 @@ no = int(input("\n Enter The No Of Student : "))
 
 i = 0
 
-if no < 3:
+if no < 2:
     print("Give 3 Or More Student Only")
 else:
     for i in range(no):
@@ -41,8 +41,17 @@ else:
 
         student.append([name,sub1,sub2,sub3,sub4,sub5,total,per,grade])
 
+sorted =student.sort(key=lambda x:x[7],reverse=True)
 
+for i in sorted:
+     if i == 100.00:
+          rank = rank+1
+     elif i == i+1: 
+          rank = rank
+     else:
+          rank = rank+1
 
 print(f"{"Name":<10}{"Sub1":<10}{"Sub2":<10}{"Sub3":<10}{"Sub4":<10}{"Sub5":<10}{"Total":<10}{"Per":<10}{"Grade":<10}")
+
 for student_reco in student:
-        print(f"{student_reco[0]:<10} {student_reco[1]:<10} {student_reco[2]:<10} {student_reco[3]:<10} {student_reco[4]:<10} {student_reco[5]:<10} {student_reco[6]:<10} {student_reco[7]:<10} {student_reco[8]:<10} {student_reco[9]:<10}")
+        print(f"{student_reco[0]:<10} {student_reco[1]:<10} {student_reco[2]:<10} {student_reco[3]:<10} {student_reco[4]:<10} {student_reco[5]:<10} {student_reco[6]:<10} {student_reco[7]:<10} {student_reco[8]:<10}")
